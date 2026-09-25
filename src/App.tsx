78f0686375d76bd97a799ed8b13ed31201c07dcc
marioacostacurbelo-sketch <marioacostacurbelo@gmail.com>
2026-09-25 23:34:50 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -722,25 +722,6 @@ function TaskForm({
 
       <div className="form-grid">
         <label>
-          
-<label>
-  Tiempo estimado
-  <select
-    value={estimatedMinutes}
-    onChange={(event) =>
-      setEstimatedMinutes(Number(event.target.value))
-    }
-  >
-    <option value={15}>15 minutos</option>
-    <option value={30}>30 minutos</option>
-    <option value={45}>45 minutos</option>
-    <option value={60}>1 hora</option>
-    <option value={90}>1 hora 30 minutos</option>
-    <option value={120}>2 horas</option>
-    <option value={150}>2 horas 30 minutos</option>
-    <option value={180}>3 horas</option>
-  </select>
-</label>
           Tarea
           <input
             value={title}
@@ -782,6 +763,24 @@ function TaskForm({
             <option>Alta</option>
           </select>
         </label>
+        <label>
+  Tiempo estimado
+  <select
+    value={estimatedMinutes}
+    onChange={(event) =>
+      setEstimatedMinutes(Number(event.target.value))
+    }
+  >
+    <option value={15}>15 minutos</option>
+    <option value={30}>30 minutos</option>
+    <option value={45}>45 minutos</option>
+    <option value={60}>1 hora</option>
+    <option value={90}>1 hora 30 minutos</option>
+    <option value={120}>2 horas</option>
+    <option value={150}>2 horas 30 minutos</option>
+    <option value={180}>3 horas</option>
+  </select>
+</label>
       </div>
 
       <div className="form-actions">
